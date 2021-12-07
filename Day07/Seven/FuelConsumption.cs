ddd0488359475fb0dd97ea3ee528b3949b06f7a7
Mali Tabatabaie <Mali.Tabatabaie@thetrainline.com>
2021-12-07 19:28:30 +0000

    Add day7

---
+++ Day07/Seven/FuelConsumption.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven
+{
+    public class FuelConsumption
+    {
+        public Tuple<int,int> CalculateFuel()
+        {
+            var testInput = new int[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+            // for this test the position 2 with cost of 37
+
+            var puzzleInput = File.ReadAllText("PuzzleInput.txt").Split(",").Select(n => int.Parse(n)).ToArray();
+            // First part: 364898
+            // second: 104149091
+
+            var leastFuelBenchmarkConstantRate = GetTotalFuelPerPositionConstantRate(puzzleInput, puzzleInput[0]);
+
+            foreach (var item in puzzleInput)
+            {
+                int totalFuelPerPositionConstantRate = GetTotalFuelPerPositionConstantRate(puzzleInput, item);
+                if (leastFuelBenchmarkConstantRate > totalFuelPerPositionConstantRate)
+                {
+                    leastFuelBenchmarkConstantRate = totalFuelPerPositionConstantRate;
+                }
+
+            }
+
+            var leastBenchMarkExpo = GetTotalFuelPerPositionExponentialRate(puzzleInput, puzzleInput[0]);
+            foreach (var item in puzzleInput)
+            {
+                var secPart = GetTotalFuelPerPositionExponentialRate(puzzleInput, item);
+                if (secPart < leastBenchMarkExpo)
+                {
+                    leastBenchMarkExpo = secPart;
+                }
+
+            }
+
+            return Tuple.Create(leastFuelBenchmarkConstantRate, leastBenchMarkExpo);
+        }
+
+        int CalcFuel(int diff)
+        {
+            return (diff * (diff + 1)) / 2;
+        }
+
+        private int GetTotalFuelPerPositionExponentialRate(int[] allPositions, int targetPosition)
+        {
+            var count = 0;
+            foreach (var position in allPositions)
+            {
+                var diff = position - targetPosition;
+                
+                if (diff < 0) { diff *= -1; }
+                
+                count += CalcFuel(diff);
+            }
+
+            return count;
+        }
+
+        private int GetTotalFuelPerPositionConstantRate(int[] allPositions, int targetPosition)
+        {
+            var count = 0;
+            foreach (var position in allPositions)
+            {
+                var diff = position - targetPosition;
+                if (diff < 0) { diff *= -1; }
+                count += diff;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Day07/Seven/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Seven;
+var fc = new FuelConsumption();
+Console.WriteLine("First part: "+ fc.CalculateFuel().Item1);
+
+Console.WriteLine("Second part: " + fc.CalculateFuel().Item2);
+
